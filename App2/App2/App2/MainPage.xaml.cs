f5a886592bd2bbaec214907f85a168e7074268f9
AndrewArs <andre-arsyono@yandex.ru>
2017-09-29 14:04:48 +0300

    Entire
    Can't debug it coz android emulator not working

---
+++ App2/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        const string uri = "http://10.0.2.2";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            GetTimeButton.Clicked += GetTimeButton_Clicked;
+        }
+
+        private void GetTimeButton_Clicked(object sender, EventArgs e)
+        {
+            string date = DateTime.Now.ToString();
+            DateTimeLabel.Text = date;
+            SendDate(date);
+        }
+
+        private async void SendDate(string Date)
+        {
+            using (var client = new HttpClient())
+            {
+                //client.BaseAddress = new Uri(uri);
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("date", Date)
+                });
+
+                var result = await client.PostAsync(uri + "/api/values/", content);
+            }
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,114 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using WebApplication1.Models;
+using System.Web.Http.Description;
+
+namespace WebApplication1.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        private DateContext db = new DateContext();
+
+        // GET: api/Values
+        public IQueryable<Date> GetDates()
+        {
+            return db.Dates;
+        }
+
+        // GET: api/Values/5
+        [ResponseType(typeof(Date))]
+        public IHttpActionResult GetDate(int id)
+        {
+            Date date = db.Dates.Find(id);
+            if (date == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(date);
+        }
+
+        // PUT: api/Values/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDate(int id, Date date)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != date.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(date).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public IHttpActionResult PostDate(string date)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            int _id = db.Dates.Max(d => d.Id) + 1;
+            db.Dates.Add(new Models.Date() { _date = date, Id = _id});
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = _id }, date);
+        }
+
+        // DELETE: api/Values/5
+        [ResponseType(typeof(Date))]
+        public IHttpActionResult DeleteDate(int id)
+        {
+            Date date = db.Dates.Find(id);
+            if (date == null)
+            {
+                return NotFound();
+            }
+
+            db.Dates.Remove(date);
+            db.SaveChanges();
+
+            return Ok(date);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DateExists(int id)
+        {
+            return db.Dates.Count(e => e.Id == id) > 0;
+        }
+    }
+}
